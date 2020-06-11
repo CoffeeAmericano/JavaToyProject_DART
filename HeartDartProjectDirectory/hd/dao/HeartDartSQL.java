0098a1e064a387e0260e9355107ee17d750056b2
RockSSGChae <52418623+RokcSSGChae@users.noreply.github.com>
2020-06-11 22:43:03 +0900

    검색기능 관련 SQL 수정

--- HeartDartProjectDirectory/hd/dao/HeartDartSQL.java
+++ HeartDartProjectDirectory/hd/dao/HeartDartSQL.java
@@ -4,5 +4,18 @@ public interface HeartDartSQL {
 
 	String company_selectall = "SELECT * FROM COMPANY_LIST ORDER BY COMPANYNAME";
 
-	String company_select = "SELECT * FROM COMPANY_LIST WHERE ? LIKE '%?%'" + "ORDER BY COMPANYNAME";
+	String company_searchAll = "SELECT * FROM COMPANY_LIST "
+	         + "WHERE COM_NAME LIKE '%' || ? || '%' "
+	         + "OR INDUSTRY_NAME LIKE '%' || ? || '%' "
+	         + "OR COM_CODE LIKE '%' || ? || '%' " 
+	         + "ORDER BY COM_NAME";
+
+	String company_searchName = "SELECT * FROM COMPANY_LIST "
+			+ "WHERE COM_NAME  LIKE '%' || ? || '%'";
+	
+	String company_searchCode = "SELECT * FROM COMPANY_LIST "
+			+ "WHERE COM_CODE  LIKE '%' || ? || '%'";
+	
+	String company_searchIndustry = "SELECT * FROM COMPANY_LIST "
+			+ "WHERE INDUSTRY_NAME  LIKE '%' || ? || '%'";
 };
