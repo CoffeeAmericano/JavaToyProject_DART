207b31abec4ae8dc90f5b809da5fe0b2266a6176
RockSSGChae <52418623+RokcSSGChae@users.noreply.github.com>
2020-06-12 19:53:25 +0900

    controller 추가

--- HeartDartProjectDirectory/hd/controller/HeartDartController.java
+++ HeartDartProjectDirectory/hd/controller/HeartDartController.java
@@ -1,5 +1,22 @@
 package hd.controller;
 
+import java.awt.EventQueue;
+
+import hd.view.Menu1;
+
 public class HeartDartController {
 
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Menu1 frame = new Menu1();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 }
+
