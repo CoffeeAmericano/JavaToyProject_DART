cb3999624cb5b101176ecac75237c7c20470bf32
RockSSGChae <52418623+RokcSSGChae@users.noreply.github.com>
2020-06-12 19:56:21 +0900

    메뉴 정리 및 수정

--- HeartDartProjectDirectory/hd/view/Menu1.java
+++ HeartDartProjectDirectory/hd/view/Menu1.java
@@ -1,172 +1,199 @@
 package hd.view;
 
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
+import hd.biz.InfoBIZ;
 import hd.biz.SearchBIZ;
+import hd.vo.InfoVO;
 
-public class Menu1 extends JFrame {
-	
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Menu1 extends JFrame implements MouseListener {
 
 	private JPanel contentPane;
-	   private JTextField textField;
-	   private JTable table;
+	private JPanel main;
+	private JPanel periodBar;
 
-	   /**
-	    * Launch the application.
-	    */
-	   public static void main(String[] args) {
-	      EventQueue.invokeLater(new Runnable() {
-	         public void run() {
-	            try {
-	               Menu1 frame = new Menu1();
-	               frame.setVisible(true);
-	            } catch (Exception e) {
-	               e.printStackTrace();
-	            }
-	         }
-	      });
-	   }
+	private Label quarterLabel;
+	private Label yearLabel;
+	private Label tipLabel;
 
-	   /**
-	    * Create the frame.
-	    */
-	   public Menu1() {
-	      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	      setIconImage(Toolkit.getDefaultToolkit().getImage(Menu1.class.getResource("/hd/view/HeartDart.png")));
-	      setTitle("\u2665Heart Dart\u2665 - Search");
-	      setBounds(100, 100, 481, 319);
-	      contentPane = new JPanel();
-	      contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-	      setContentPane(contentPane);
-	      contentPane.setLayout(null);
-	      
-	      
-	      
-	      JPanel p = new JPanel();
-	      p.setBounds(0, 235, 463, 37);
-	      p.setBackground(new Color(205, 92, 92));
-	      contentPane.add(p);
-	      
-	      JComboBox combo = new JComboBox(new Object[]{});
-	      combo.setBackground(Color.WHITE);
-	      combo.setFont(new Font("경기천년바탕 Regular", Font.BOLD, 17));
-	      combo.setModel(new DefaultComboBoxModel(new String[] {"  \uC804\uCCB4", "  \uD68C\uC0AC\uBA85 ", "  \uCF54\uB4DC", "  \uC5C5\uC885"}));
-	      p.add(combo);
-	      
-	      textField = new JTextField(20);
-	      p.add(textField);
-	      
-	           
-	      JPanel panel = new JPanel();
-	      panel.setBackground(Color.DARK_GRAY);
-	      panel.setBounds(0, 0, 463, 30);
-	      contentPane.add(panel);
-	      panel.setLayout(null);
-	      
-	      Choice choice = new Choice();
-	      choice.setBounds(299, 3, 52, 24);
-	      panel.add(choice);
-	      
-	      Label label = new Label("\uBD84\uAE30 :");
-	      label.setAlignment(Label.CENTER);
-	      label.setForeground(new Color(255, 255, 255));
-	      label.setFont(new Font("Dialog", Font.BOLD, 15));
-	      label.setBounds(243, 3, 52, 25);
-	      panel.add(label);
-	      
-	      Label label_1 = new Label("\uB144\uB3C4 :");
-	      label_1.setForeground(Color.WHITE);
-	      label_1.setFont(new Font("Dialog", Font.BOLD, 15));
-	      label_1.setAlignment(Label.CENTER);
-	      label_1.setBounds(123, 4, 53, 25);
-	      panel.add(label_1);
-	      
-	      Choice choice_1 = new Choice();
-	      choice_1.setBounds(181, 3, 52, 24);
-	      panel.add(choice_1);
-	      
-	      Label label_2 = new Label("*\uAE30\uAC04\uC120\uD0DD*");
-	      label_2.setFont(new Font("Dialog", Font.BOLD, 15));
-	      label_2.setForeground(new Color(255, 255, 255));
-	      label_2.setAlignment(Label.CENTER);
-	      label_2.setBounds(18, 4, 92, 25);
-	      panel.add(label_2);
-	      
-	      Button button = new Button("Next");
-	      button.setBackground(new Color(255, 255, 255));
-	      button.setForeground(new Color(205, 92, 92));
-	      button.setBounds(366, 3, 87, 25);
-	      panel.add(button);
-	      
-	      JScrollPane scrollPane = new JScrollPane();
-	      scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-	      scrollPane.setBounds(0, 31, 463, 206);
-	      contentPane.add(scrollPane);
-	      
-	      String[] name = {"회사명", "업종명", "종목코드"};
-	      DefaultTableModel dt = new DefaultTableModel(name, 0){
-	    	  public boolean isCellEditable(int i, int c){
-	              return false;
-	             }
-	      };
-	      
-	      table = new JTable(dt);
-	      scrollPane.setViewportView(table);
+	private Choice quarterChoice;
+	private Choice yearChoice;
 
-	      // 회사 테이블을 첫 화면에 띄우기 
-	      // 화면에 모든 회사 목록 띄우기 -> BIZ호출
-	      SearchBIZ.userSelectAll(dt);
-	      
-	      if (dt.getRowCount() > 0)
-	         table.setRowSelectionInterval(0, 0);
-	      
-	      // 유저 조건 서치
-		JButton serach = new JButton("\uAC80\uC0C9");
-		p.add(serach);
-		serach.addMouseListener(new MouseAdapter() {
-			
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				switch (combo.getSelectedItem().toString().trim()) {
-				case "전체" :
-					new SearchBIZ().userSearchAll(dt, textField.getText());
-					break;
-				case "회사명" :
-					new SearchBIZ().userSearch(dt, "com_name",textField.getText());
-					break;
-				case "코드" :
-					new SearchBIZ().userSearch(dt, "com_code",textField.getText());
-					break;
-				case "업종" :
-					new SearchBIZ().userSearch(dt, "industry_name",textField.getText());
-					break;
-				}
+	private JTextField searchTextField;
+	private JTable table;
+	private DefaultTableModel dt;
+	@SuppressWarnings("rawtypes")
+	private JComboBox searchCombo;
+	private JButton searchButton;
+	private JScrollPane scrollPane;
+	
+	private Map<String, ArrayList<String>> info_str;
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public Menu1() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Menu2.class.getResource("/hd/view/HeartDart.png")));
+		setTitle("\u2665Heart Dart\u2665 - Search");
+		setBounds(100, 100, 481, 319);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		main = new JPanel();
+		main.setBounds(0, 235, 463, 37);
+		main.setBackground(new Color(205, 92, 92));
+		contentPane.add(main);
+
+		searchCombo = new JComboBox(new Object[] {});
+		searchCombo.setBackground(Color.WHITE);
+		searchCombo.setFont(new Font("경기천년바탕 Regular", Font.BOLD, 17));
+		searchCombo.setModel(new DefaultComboBoxModel(
+				new String[] { "  \uC804\uCCB4", "  \uD68C\uC0AC\uBA85 ", "  \uCF54\uB4DC", "  \uC5C5\uC885" }));
+		main.add(searchCombo);
+
+		searchTextField = new JTextField(20);
+		main.add(searchTextField);
+
+		periodBar = new JPanel();
+		periodBar.setBackground(Color.DARK_GRAY);
+		periodBar.setBounds(0, 0, 463, 30);
+		contentPane.add(periodBar);
+		periodBar.setLayout(null);
+
+		quarterLabel = new Label("\uBD84\uAE30 :");
+		quarterLabel.setAlignment(Label.CENTER);
+		quarterLabel.setForeground(new Color(255, 255, 255));
+		quarterLabel.setFont(new Font("Dialog", Font.BOLD, 15));
+		quarterLabel.setBounds(243, 3, 52, 25);
+		periodBar.add(quarterLabel);
+
+		yearLabel = new Label("\uB144\uB3C4 :");
+		yearLabel.setForeground(Color.WHITE);
+		yearLabel.setFont(new Font("Dialog", Font.BOLD, 15));
+		yearLabel.setAlignment(Label.CENTER);
+		yearLabel.setBounds(104, 3, 53, 25);
+		periodBar.add(yearLabel);
+
+		quarterChoice = new Choice();
+		quarterChoice.setBounds(299, 3, 52, 24);
+		quarterChoice.add("1");
+		quarterChoice.add("2");
+		quarterChoice.add("3");
+		quarterChoice.add("4");
+		periodBar.add(quarterChoice);
+
+		yearChoice = new Choice();
+		yearChoice.setBounds(163, 3, 74, 24);
+		yearChoice.add("2015");
+		yearChoice.add("2016");
+		yearChoice.add("2017");
+		yearChoice.add("2018");
+		yearChoice.add("2019");
+		yearChoice.add("2020");
+		periodBar.add(yearChoice);
+
+		tipLabel = new Label("*\uAE30\uAC04\uC120\uD0DD*");
+		tipLabel.setFont(new Font("Dialog", Font.BOLD, 15));
+		tipLabel.setForeground(new Color(255, 255, 255));
+		tipLabel.setAlignment(Label.CENTER);
+		tipLabel.setBounds(18, 4, 92, 25);
+		periodBar.add(tipLabel);
+
+		scrollPane = new JScrollPane();
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setBounds(0, 31, 463, 206);
+		contentPane.add(scrollPane);
+
+		searchButton = new JButton("\uAC80\uC0C9");
+		main.add(searchButton);
+
+		String[] name = { "회사명", "업종명", "종목코드" };
+		dt = new DefaultTableModel(name, 0) {
+			public boolean isCellEditable(int i, int c) {
+				return false;
 			}
-		});
-		// 더블클릭 Info 확인
-		table.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mousePressed(MouseEvent e) {
-				JTable jt = (JTable)e.getSource();
-				Point point = e.getPoint();
-				int row = jt.rowAtPoint(point);
-				if(e.getClickCount() == 2 && jt.getSelectedRow() != -1) {
-					String clickCode = (String) jt.getValueAt(row, 2);
-					String period = yearChoice.getSelectedItem().substring(2, 4) + "_" +
-							quarterChoice.getSelectedItem();
-					InfoVO infoVO = new InfoBIZ().infoSelect(clickCode, period);
-					Page page = new Page(infoVO);
-				}
+		};
+		
+		table = new JTable(dt);
+		scrollPane.setViewportView(table);
+
+		
+
+		// 회사 테이블을 첫 화면에 띄우기
+		// 화면에 모든 회사 목록 띄우기 -> BIZ호출
+		SearchBIZ.userSelectAll(dt);
+
+		if (dt.getRowCount() > 0)
+			table.setRowSelectionInterval(0, 0);
+
+		// 유저 조건 서치
+
+		searchButton.addMouseListener(this);
+		table.addMouseListener(this);
+
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if (e.getSource() == searchButton) {
+			switch (searchCombo.getSelectedItem().toString().trim()) {
+			case "전체":
+				new SearchBIZ().userSearchAll(dt, searchTextField.getText());
+				break;
+			case "회사명":
+				new SearchBIZ().userSearch(dt, "com_name", searchTextField.getText());
+				break;
+			case "코드":
+				new SearchBIZ().userSearch(dt, "com_code", searchTextField.getText());
+				break;
+			case "업종":
+				new SearchBIZ().userSearch(dt, "industry_name", searchTextField.getText());
+				break;
 			}
-		});   
+		} else if (e.getSource() == table) {
+			JTable jt = (JTable) e.getSource();
+			Point point = e.getPoint();
+			int row = jt.rowAtPoint(point);
+			if (e.getClickCount() == 2 && jt.getSelectedRow() != -1) {
+				String clickCode = (String) jt.getValueAt(row, 2);
+				String period = yearChoice.getSelectedItem().substring(2, 4) + "_" + quarterChoice.getSelectedItem();
+				InfoVO infoVO = new InfoBIZ().infoSelect(clickCode, period);
+				System.out.println(infoVO.getBs());
 
+				Page page = new Page();
+				page.setInfoVO(infoVO);
+				info_str = page.parseInfo(page.getInfoVO());
+			}
+		}
+	}
 
-	      
-	   }
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}
 }
