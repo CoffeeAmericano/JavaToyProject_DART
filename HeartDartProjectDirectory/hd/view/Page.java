eed600b63e29cf49a62c7c907dbeca5962be5df8
rodemrayl <53180757+rodemrayl@users.noreply.github.com>
2020-06-12 12:24:05 +0900

    Update Page.java

--- HeartDartProjectDirectory/hd/view/Page.java
+++ HeartDartProjectDirectory/hd/view/Page.java
@@ -4,6 +4,10 @@ import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
+import hd.dao.SearchDAO;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 public class Page extends JFrame {
 
 	private JPanel contentPane;
@@ -34,85 +38,151 @@ public class Page extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1075, 622);
 		contentPane = new JPanel();
-		contentPane.setBackground(new Color(128, 128, 128));
+		contentPane.setBackground(Color.DARK_GRAY);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		Panel panel = new Panel();
-		panel.setBackground(Color.DARK_GRAY);
+		panel.setBackground(Color.BLACK);
 		panel.setBounds(0, 0, 234, 575);
 		contentPane.add(panel);
 		panel.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon(Page.class.getResource("/Page_1/heartTest2.png")));
+		lblNewLabel.setIcon(new ImageIcon(Page.class.getResource("/hd/view/heartTest2.png")));
 		lblNewLabel.setForeground(new Color(255, 255, 255));
 		lblNewLabel.setFont(new Font("굴림", Font.BOLD, 30));
 		lblNewLabel.setBounds(-62, 35, 282, 145);
 		panel.add(lblNewLabel);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("Are you");
 		lblNewLabel_2.setFont(new Font("굴림", Font.BOLD, 29));
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_2.setForeground(Color.WHITE);
 		lblNewLabel_2.setBounds(14, 204, 206, 46);
 		panel.add(lblNewLabel_2);
-		
+
 		JLabel lblNewLabel_2_1 = new JLabel("New");
 		lblNewLabel_2_1.setFont(new Font("굴림", Font.BOLD, 30));
 		lblNewLabel_2_1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_2_1.setForeground(Color.WHITE);
 		lblNewLabel_2_1.setBounds(14, 262, 206, 46);
 		panel.add(lblNewLabel_2_1);
-		
+
 		JLabel lblNewLabel_2_2 = new JLabel("to finance?");
 		lblNewLabel_2_2.setFont(new Font("굴림", Font.BOLD, 30));
 		lblNewLabel_2_2.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_2_2.setForeground(Color.WHITE);
 		lblNewLabel_2_2.setBounds(14, 320, 206, 46);
 		panel.add(lblNewLabel_2_2);
-		
+
 		JLabel lblNewLabel_3 = new JLabel("");
 		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_3.setIcon(new ImageIcon(Page.class.getResource("/SecPage/chart3.png")));
+		lblNewLabel_3.setIcon(new ImageIcon(Page.class.getResource("/hd/view/chart3.png")));
 		lblNewLabel_3.setBounds(14, 370, 206, 193);
 		panel.add(lblNewLabel_3);
-		
+
 		Panel panel_1 = new Panel();
 		panel_1.setBackground(Color.WHITE);
-		panel_1.setBounds(255, 28, 383, 327);
+		panel_1.setBounds(255, 28, 383, 333);
 		contentPane.add(panel_1);
 		panel_1.setLayout(null);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("");
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setIcon(new ImageIcon(Page.class.getResource("/Page_1/logo.png")));
+		lblNewLabel_1.setIcon(new ImageIcon(Page.class.getResource("/hd/view/logo.png")));
 		lblNewLabel_1.setBounds(26, 0, 331, 327);
 		panel_1.add(lblNewLabel_1);
 		
-		Panel panel_1_1 = new Panel();
-		panel_1_1.setBackground(new Color(192, 192, 192));
-		panel_1_1.setBounds(664, 28, 372, 327);
-		contentPane.add(panel_1_1);
-		panel_1_1.setLayout(null);
+		JTabbedPane tabpane = new JTabbedPane();
+		tabpane.setBackground(Color.WHITE);
+		tabpane.setTabPlacement(JTabbedPane.RIGHT);
+		tabpane.setSize(444, 543);
+		tabpane.setLocation(644, 28);
 		
-		Panel panel_1_2_1 = new Panel();
-		panel_1_2_1.setBackground(new Color(192, 192, 192));
-		panel_1_2_1.setBounds(255, 375, 383, 176);
-		contentPane.add(panel_1_2_1);
-		panel_1_2_1.setLayout(null);
+		JPanel tab1 = new JPanel();
+		tab1.setToolTipText("");
+		tab1.setBackground(Color.WHITE);
+		tabpane.add("안전성", tab1);
+		tabpane.setBackgroundAt(0, Color.WHITE);
+		JButton btn1_1 = new JButton("수식");
+		btn1_1.setIcon(new ImageIcon(Page.class.getResource("/hd/view/HTicon1.png")));
+		btn1_1.setForeground(new Color(205, 92, 92));
+		btn1_1.setFont(new Font("경기천년제목 Bold", Font.BOLD, 20));
+		btn1_1.setBackground(Color.WHITE);
+		btn1_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		JButton btn1_2 = new JButton("설명");
+		btn1_2.setForeground(new Color(205, 92, 92));
+		btn1_2.setFont(new Font("경기천년제목 Bold", Font.PLAIN, 20));
+		btn1_2.setIcon(new ImageIcon(Page.class.getResource("/hd/view/HTicon2.png")));
+		JButton btn1_3 = new JButton("Tip");
+		btn1_3.setForeground(new Color(205, 92, 92));
+		btn1_3.setFont(new Font("경기천년제목 Bold", Font.BOLD, 20));
+		btn1_3.setIcon(new ImageIcon(Page.class.getResource("/hd/view/HTicon3.png")));
+		JTextField txt_1 = new JTextField("단어를 입력해보세요", 25);
+		tab1.add(btn1_1);
+		tab1.add(btn1_2);
+		tab1.add(btn1_3);
+		tab1.add(txt_1);
 		
-		Button button_1 = new Button("\uC8FC\uC11D");
-		button_1.setBounds(261, 108, 112, 58);
-		panel_1_2_1.add(button_1);
-		button_1.setFont(new Font("Georgia", Font.BOLD, 20));
-		button_1.setForeground(new Color(255, 255, 255));
-		button_1.setBackground(new Color(255, 99, 71));
+		JPanel tab2 = new JPanel();
+		tabpane.add("성장성", tab2);
+		tabpane.setBackgroundAt(1, Color.PINK);
+		JButton btn2_1 = new JButton("수식");
+		JButton btn2_2 = new JButton("설명");
+		JButton btn2_3 = new JButton("Tip");
+		JTextField txt_2 = new JTextField("단어를 입력해보세요", 25);
+		tab2.add(btn2_1);
+		tab2.add(btn2_2);
+		tab2.add(btn2_3);
+		tab2.add(txt_2);
 		
-		Panel panel_2 = new Panel();
-		panel_2.setBackground(new Color(192, 192, 192));
-		panel_2.setBounds(664, 378, 372, 173);
-		contentPane.add(panel_2);
+		JPanel tab3 = new JPanel();
+		tabpane.add("수익성", tab3);
+		JButton btn3_1 = new JButton("수식");
+		JButton btn3_2 = new JButton("설명");
+		JButton btn3_3 = new JButton("Tip");
+		JTextField txt_3 = new JTextField("단어를 입력해보세요", 25);
+		tab3.add(btn3_1);
+		tab3.add(btn3_2);
+		tab3.add(btn3_3);
+		tab3.add(txt_3);
+		
+		JPanel tab4 = new JPanel();
+		tabpane.add("활동성", tab4);
+		JButton btn4_1 = new JButton("수식");
+		JButton btn4_2 = new JButton("설명");
+		JButton btn4_3 = new JButton("Tip");
+		JTextField txt_4 = new JTextField("단어를 입력해보세요", 25);
+		tab3.add(btn4_1);
+		tab3.add(btn4_2);
+		tab3.add(btn4_3);
+		tab3.add(txt_4);
+		
+		JPanel tab5 = new JPanel();
+		tabpane.add("활동성", tab5);
+		JButton btn5_1 = new JButton("수식");
+		JButton btn5_2 = new JButton("설명");
+		JButton btn5_3 = new JButton("Tip");
+		JTextField txt_5 = new JTextField("단어를 입력해보세요", 25);
+		tab3.add(btn5_1);
+		tab3.add(btn5_2);
+		tab3.add(btn5_3);
+		tab3.add(txt_5);
+		
+		
+		getContentPane().add(tabpane);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(255, 371, 377, 200);
+		contentPane.add(scrollPane);
+		setSize(1120, 630);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
