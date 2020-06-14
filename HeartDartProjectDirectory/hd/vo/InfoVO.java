c3e8b84999494738f5ee4a403c5ae489dd3ac2a2
RockSSGChae <52418623+RokcSSGChae@users.noreply.github.com>
2020-06-14 21:36:51 +0900

    Update InfoVO.java

--- HeartDartProjectDirectory/hd/vo/InfoVO.java
+++ HeartDartProjectDirectory/hd/vo/InfoVO.java
@@ -2,42 +2,73 @@ package hd.vo;
 
 public class InfoVO {
 	private String comCode;
-	private String bs;
-	private String is;
-	private String cf;
+	private String bs_last;
+	private String is_last;
+	private String cf_last;
+	private String bs_now;
+	private String is_now;
+	private String cf_now;
+	
 	
 	public InfoVO() {
 		super();
+		// TODO Auto-generated constructor stub
 	}
-	public InfoVO(String comCode, String bs, String is, String cf) {
+	
+	public InfoVO(String comCode, String bs_last, String is_last, String cf_last, String bs_now, String is_now,
+			String cf_now) {
 		super();
 		this.comCode = comCode;
-		this.bs = bs;
-		this.is = is;
-		this.cf = cf;
+		this.bs_last = bs_last;
+		this.is_last = is_last;
+		this.cf_last = cf_last;
+		this.bs_now = bs_now;
+		this.is_now = is_now;
+		this.cf_now = cf_now;
 	}
+
 	public String getComCode() {
 		return comCode;
 	}
 	public void setComCode(String comCode) {
 		this.comCode = comCode;
 	}
-	public String getBs() {
-		return bs;
+	public String getBs_now() {
+		return bs_now;
 	}
-	public void setBs(String bs) {
-		this.bs = bs;
+	public void setBs_now(String bs_now) {
+		this.bs_now = bs_now;
 	}
-	public String getIs() {
-		return is;
+	public String getIs_now() {
+		return is_now;
 	}
-	public void setIs(String is) {
-		this.is = is;
+	public void setIs_now(String is_now) {
+		this.is_now = is_now;
 	}
-	public String getCf() {
-		return cf;
+	public String getCf_now() {
+		return cf_now;
 	}
-	public void setCf(String cf) {
-		this.cf = cf;
+	public void setCf_now(String cf_now) {
+		this.cf_now = cf_now;
 	}
+	public String getBs_last() {
+		return bs_last;
+	}
+	public void setBs_last(String bs_last) {
+		this.bs_last = bs_last;
+	}
+	public String getIs_last() {
+		return is_last;
+	}
+	public void setIs_last(String is_last) {
+		this.is_last = is_last;
+	}
+	public String getCf_last() {
+		return cf_last;
+	}
+	public void setCf_last(String cf_last) {
+		this.cf_last = cf_last;
+	}
+	
 }
+
